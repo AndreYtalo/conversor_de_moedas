fa27d51e26fdfd6750ee7de2a0200e2b9c91f3e2
AndreYtalo <andreytalo6@gmail.com>
2023-04-07 20:28:19 -0300

    add tela na main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:conversor_de_moedas/converso/tela.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: ,));
+  runApp(const MaterialApp(
+    home: conversor_de_moedas(),
+  ));
 }
-
