656e12a8e282485dc4664007a3447c7e9d943039
AndreYtalo <andreytalo6@gmail.com>
2023-04-22 18:31:20 -0300

    projeto final

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:conversor_de_moedas/animes/tela.dart';
 import 'package:conversor_de_moedas/converso/tela.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: conversor_de_moedas(),
+    home: Ytalo(),
   ));
 }
